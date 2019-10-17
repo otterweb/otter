4247a3ec226abca0958685f8c6079151bd1f49ea
Charles Rymal <charlesrymal@gmail.com>
2019-10-17 16:35:26 +0000

    dont know

--- scripts/pypi.bash
+++ scripts/pypi.bash
@@ -6,5 +6,8 @@ if [ $? -ne 0 ]; then exit 1; fi
 
 #python3.7 -m twine upload --verbose --repository-url https://test.pypi.org/legacy/ dist/*
 
-python3.7 -m twine upload --verbose dist/*
+python3.7 -m twine upload -r $1 dist/*
+
+
+
 
--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ import re
 import json
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = '0.0.1'
+
 
 kwargs = {
         'name': 'otter',
-        'version': '0',
+        'version': version,
         'description': 'Python library for interacting with Otter Web API',
+        'long_description':long_description,
         'packages': ['otter'],
         'zip_safe': False,
         }
